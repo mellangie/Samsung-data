8691a524e435b2c7f75e54d132791781ce1e5bd1
mellangie <angela03@gmail.com>
2015-11-22 17:36:38 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,45 +1,69 @@
 
-if (!file.info("UCI HAR Dataset")$isdir) {
-  dataFile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  dir.create("data")
-  download.file(dataFile, "./data/UCI-HAR-dataset.zip", method="libcurl")
-  unzip("./data/UCI-HAR-dataset.zip")
+if (!require("data.table")) {
+  install.packages("data.table")
 }
 
-
-#1
-features<-read.table("./data/features.txt")
-x_test<-read.table("./data/X_test.txt",col.names=features[,2])
-x_train<-read.table("./data/X_train.txt",col.names=features[,2])
-x<-rbind(train,test)
-
-#2
-
-measurements <- features[grep("(mean|std)\\(", features[,2]),]
-#x<-features[grep("mean|std",features$V2),]
-mean_and_std <- Datos[,measurements[,1]]
-
-#3
-y_test <- read.table("./data/y_test.txt", col.names = c('activity'))
-y_train <- read.table("./data/y_train.txt", col.names = c('activity'))
-y <- rbind(y_test, y_train)
-
-labels <- read.table("./data/activity_labels.txt")
-for (i in 1:nrow(labels)) {
-  code <- as.numeric(labels[i, 1])
-  name <- as.character(labels[i, 2])
-  y[y$activity == code, ] <- name
+if (!require("reshape2")) {
+  install.packages("reshape2")
 }
 
-#4
-X_with_labels <- cbind(y, x)
-mean_and_std_with_labels <- cbind(y, mean_and_std)
+require("data.table")
+require("reshape2")
 
-#5
-subject_test <- read.table("./data/subject_test.txt", col.names = c('subject'))
-subject_train <- read.table("./data/subject_train.txt", col.names = c('subject'))
-subject <- rbind(subject_test, subject_train)
-averages <- aggregate(x, by = list(activity = y[,1], subject = subject[,1]), mean)
+# Load: activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
-write.csv(averages, file='./data/result.txt', row.names=FALSE)
+# Load: data column names
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
+# Extract only the measurements on the mean and standard deviation for each measurement.
+extract_features <- grepl("mean|std", features)
+
+# Load and process X_test & y_test data.
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+names(X_test) = features
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+X_test = X_test[,extract_features]
+
+# Load activity labels
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+
+# Bind data
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+
+# Load and process X_train & y_train data.
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+names(X_train) = features
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+X_train = X_train[,extract_features]
+
+# Load activity data
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+
+# Bind data
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+
+# Merge test and train data
+data = rbind(test_data, train_data)
+
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+
+# Apply mean function to dataset using dcast function
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+write.table(tidy_data, file = "./tidy_data.txt")
